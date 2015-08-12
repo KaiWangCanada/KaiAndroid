53cf8ed4914804afc35840a9616b079c6575f94d
KaiWangCanada <kaiwangcanada@gmail.com>
2015-08-12 17:40:18 -0400

    guava eventbus (unfinish)

---
+++ app/src/main/java/kai/kaiprivate/thirdparty/guava/KaiGuavaEventBus.java
@@ -0,0 +1,26 @@
+package kai.kaiprivate.thirdparty.guava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import kai.kaiprivate.R;
+
+public class KaiGuavaEventBus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kai_guava_event_bus);
+
+
+
+    }
+
+    public void click(View view) {
+
+    }
+
+}
