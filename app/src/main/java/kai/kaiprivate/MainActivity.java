14c6c8c6db812f2f02bcc17ddb92d306af58794c
KaiWangCanada <kaiwangcanada@gmail.com>
2015-08-26 14:25:56 -0400

    multidex

--- app/src/main/java/kai/kaiprivate/MainActivity.java
+++ app/src/main/java/kai/kaiprivate/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 //
 //        Funcs.useApplication((KaiApplication) getApplication());
 //        Funcs.useTelephoneManager((TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE));
@@ -71,8 +71,8 @@ public class MainActivity extends ActionBarActivity {
 //KaiPhysicsLayout
 //KaiTooltipView
 //KaiShowTipView
-//KaiAppIntro
-KaiTimeSquare
+KaiAppIntro
+//KaiTimeSquare
 
 
 
